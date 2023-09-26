6e9a058ebf0b4eddb7540ca27c7e59ada5c04ed4
DeokYoung980130 <141919067+DeokYoung980130@users.noreply.github.com>
2023-09-26 19:50:00 +0900

    CustomTokenObtainPairSerializer

--- users/views.py
+++ users/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework_simplejwt.views import(
+    TokenObtainPairView
+)
 
-from users.serializers import UserSerializer
-# Create your views here.
+from users.serializers import CustomTokenObtainPairSerializer, UserSerializer
+
 
 class UserView(APIView):
     def post(self, request):
@@ -13,3 +16,6 @@ class UserView(APIView):
             return Response({"message":"가입완료"}, status=status.HTTP_201_CREATED)
         else:
             return Response({"message":f"${serializer.errors}"}, status=status.HTTP_400_BAD_REQUEST)
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
